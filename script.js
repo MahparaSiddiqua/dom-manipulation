77022f8b7f0939da890693fde5f4b9fe29014041
M_Siddiqua <mahpara.arthi@gmail.com>
2018-06-04 04:20:21 +0000

    Task 4 b

--- script.js
+++ script.js
@@ -26,3 +26,16 @@ function changeBGcolor(event){
     window.location.assign('http;//google.com')
 }
 /* Task 4 using jQuery */
+//Task 4a
+
+$('#addText4').click(function(event){
+    var paragraph = $('<p>').text('Hello World')
+    $('#task4a').append(paragraph)
+})
+
+//Task 4b
+
+function changeBG(event){
+    var color = event.target.innerText.toLowerCase();
+    $('body').css('background-color', color)
+}
